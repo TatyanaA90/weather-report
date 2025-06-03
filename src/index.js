2058b89ad7669713276c4568661d912d7cfef7c1
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-06-02 20:19:33 -0700

    Implement Wave_2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
+"use strict";
+
 let tempF = 85;
-const tempValue = document.getElementById('tempValue')
 
-const tempC = Math.round((tempF - 32) * 5 / 9);
-tempValue.textContent = `${tempF}°F ⎯ ${tempC}°C`;
+const tempValue = document.getElementById("tempValue")
+const increaseButton = document.getElementById("increaseTempControl")
+const decreaseButton = document.getElementById("decreaseTempControl");
+const landscape = document.getElementById("landscape");
 
-if (tempF >= 80) {
-    tempValue.classList.add('red');
-} else if (tempF >= 70) {
-    tempValue.classList.add('orange');
-} else if (tempF >= 60) {
-    tempValue.classList.add('yellow');
-} else if (tempF >= 50) {
-    tempValue.classList.add('green');
-} else {
-    tempValue.classList.add('teal')
-};
+function convertToCelsius(fahrenheit) {
+    return Math.round((fahrenheit - 32) * 5 / 9);
+}
+
+function updateTempColor() {
+    let tempC = convertToCelsius(tempF);
+    tempValue.textContent = tempF + "°F\n⎯\n" + tempC + "°C";
+
+    if (tempF >= 80) {
+        tempValue.classList.add("red");
+        landscape.textContent = "🏜️☀️🏖️🌞🏝️☀️🌵🔥⛱️🌤️";
+    } else if (tempF >= 70) {
+        tempValue.classList.add("orange");
+        landscape.textContent = "🌿🌻🌼🌸🌷🪻☀️🦋🐝🌤️"; 
+    } else if (tempF >= 60) {
+        tempValue.classList.add("yellow");
+        landscape.textContent = "🌳🍂🪵🍃🦉🍁🐿️🍄🍁⛅";
+    } else if (tempF >= 50) {
+        tempValue.classList.add("green");
+        landscape.textContent = "🍁🌦️🌾🌤️🐛🌼🌦️🍂🪲☘️";
+    } else {
+        tempValue.classList.add("teal");
+        landscape.textContent = "❄️⛄🌨️🌬️⛄🌲☃️❄️🥶🌨️";
+    }
+}
+
+increaseButton.addEventListener("click", function () {
+    tempF++;
+    updateTempColor();
+});
+
+decreaseButton.addEventListener("click", function () {
+    tempF--;
+    updateTempColor();
+});
+updateTempColor();
