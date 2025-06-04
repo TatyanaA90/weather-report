4698b1a0a462ffc68efe7aae663cb29d917ae4c1
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-06-03 21:01:34 -0700

    Refactor retry in Wave_

--- src/index.js
+++ src/index.js
@@ -63,51 +63,57 @@ displayCityName();
 // wave 6: // document.getElementById("cityNameInput").value = "";
 // retry helper func with attemps but not nessasary for this project
 // wave 4: //
-const waitAttempt = (request, attempt = 1) => {
-    return request().catch((error) => {
-        if (attempt >= 3) {
-            console.log("Max attempts reached");
-            return null;
+
+const getWithRetry = (url, queryParams, attempt = 1) => {
+    return axios.get(url, {
+        params: queryParams
+    }).then((response) => {
+        const data = response.data;
+        // Since our proxy always responds with status code 200, 
+        // we need another way to detect when to retry.
+        // After some research, it turns out that if data.error is not empty, 
+        // it indicates we should retry.
+        if (data.error) {
+            if (attempt >= 5) {
+                console.log("Max attempts reached!");
+                return null;
+            }
+            console.log(`Empty response on attempt ${attempt}. Retrying...`);
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(getWithRetry(url, queryParams, attempt + 1));
+                }, 600 * attempt);
+            });
         }
-        console.log(error);
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(waitAttempt(request, attempt + 1));
-            }, 200 * attempt);
-        });
-    });
+
+        return response;
+    })
 };
 
 const findLatitudeAndLongitude = (query) => {
-    const request = () => {
-        return axios.get('http://localhost:5000/location', {
-            params: {
-                q: query,
-                format: 'json'
-            }
-        }).then((response) => {
-            const { lat: latitude, lon: longitude } = response.data[0];
-            return { latitude, longitude };
-        });
-    };
-
-    return waitAttempt(request);
+    const url = "http://localhost:5000/location";
+    return getWithRetry(url, { q: query, format: 'json' }).then((response) => {
+        if (!response.data) {
+            return null;
+        }
+        const { lat: latitude, lon: longitude } = response.data[0];
+        return { latitude, longitude };
+    });
 };
-
 const findWeatherLatLon = (lat, lon) => {
-    const request = () => {
-        return axios.get("http://localhost:5000/weather", {
-            params: { lat, lon }
-        }).then((response) => {
-            const tempK = response.data.main.temp;
-            const tempC = Math.round(tempK - 273.15);
-            const tempF = Math.round((tempC * 9) / 5 + 32);
-            return { tempF, tempC };
-        });
-    };
+    const url = "http://localhost:5000/weather";
 
-    return waitAttempt(request);
+    return getWithRetry(url, { lat, lon }).then((response) => {
+        if (!response.data) {
+            return null;
+        }
+        const tempK = response.data.main.temp;
+        const tempC = Math.round(tempK - 273.15);
+        const tempF = Math.round((tempC * 9) / 5 + 32);
+        return { tempF, tempC };
+    });
 };
+
 
 currentTempButton.addEventListener("click", () => {
     const city = cityNameElement.textContent.trim();
